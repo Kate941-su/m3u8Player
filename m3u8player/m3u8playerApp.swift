11cffcddfd5c096cb96fb7f707dfbf063837ef9c
Kaito Kitaya <kaito.kitaya.deutschland@gmail.com>
2024-10-03 20:58:22 +0200

    Initial Commit

---
+++ m3u8player/m3u8playerApp.swift
@@ -0,0 +1,17 @@
+//
+//  m3u8playerApp.swift
+//  m3u8player
+//
+//  Created by Kaito Kitaya on 03.10.24.
+//
+
+import SwiftUI
+
+@main
+struct m3u8playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ m3u8playerUITests/m3u8playerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  m3u8playerUITestsLaunchTests.swift
+//  m3u8playerUITests
+//
+//  Created by Kaito Kitaya on 03.10.24.
+//
+
+import XCTest
+
+final class m3u8playerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
